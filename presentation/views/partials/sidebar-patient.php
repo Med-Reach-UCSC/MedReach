f6f78215a79f5f395c08ecf30277412e29bf3b55
Thisuni Rupasinghe <thisunirupasinghe@gmail.com>
2026-09-24 09:29:50 +0530

    feat(patient): add collapsible sidebar with persisted state

--- presentation/views/partials/sidebar-patient.php
+++ presentation/views/partials/sidebar-patient.php
@@ -7,19 +7,29 @@ $active = $active ?? 'dashboard';
 $mr_sidebar_links = [
   'dashboard'     => ['label' => 'Dashboard',     'icon' => 'home',        'href' => 'patient-dashboard.php'],
   'prescriptions' => ['label' => 'Prescriptions',  'icon' => 'clipboard', 'href' => '#'],
+  'family'        => ['label' => 'Family',         'icon' => 'family',      'href' => 'guardian-dashboard.php'],
   'orders'        => ['label' => 'Orders',         'icon' => 'list',        'href' => 'patient-order.php'],
   'pharmacies'    => ['label' => 'Pharmacies',     'icon' => 'shop',        'href' => 'nearby-pharmacies.php'],
   'payments'      => ['label' => 'Payments',       'icon' => 'cash',        'href' => 'payments.php'],
 ];
 ?>
 <aside class="mr-sidebar" aria-label="Patient navigation">
-  <a class="mr-sidebar__brand" href="patient-dashboard.php" aria-label="MedReach">
-    <img src="presentation/assets/images/logo-symbol.png" alt="">
-  </a>
+  <div class="mr-sidebar__top">
+    <a class="mr-sidebar__brand" href="patient-dashboard.php" aria-label="MedReach">
+      <span class="mr-sidebar__brand-icon">
+        <img src="presentation/assets/images/logo-symbol.png" alt="">
+      </span>
+      <span class="mr-sidebar__brand-text">MedReach</span>
+    </a>
+    <button type="button" class="mr-sidebar__collapse" aria-label="Collapse menu" aria-pressed="false">
+      <img class="mr-sidebar__collapse-icon mr-sidebar__collapse-icon--collapse" src="https://img.icons8.com/ios-filled/50/454655/chevron-left.png" alt="">
+      <img class="mr-sidebar__collapse-icon mr-sidebar__collapse-icon--expand" src="https://img.icons8.com/ios-filled/50/454655/chevron-right.png" alt="">
+    </button>
+  </div>
 
   <a class="mr-notif-btn mr-notif-btn--sidebar" href="notifications.php" aria-label="Notifications">
-     <img src="https://img.icons8.com/ios-filled/50/1a1b24/appointment-reminders.png" alt="">
-  <span class="mr-notif-btn__dot" aria-hidden="true"></span>
+    <img src="https://img.icons8.com/ios-filled/50/1a1b24/appointment-reminders.png" alt="">
+    <span class="mr-notif-btn__dot" aria-hidden="true"></span>
   </a>
 
   <button type="button" class="mr-sidebar__toggle" aria-label="Toggle menu" aria-expanded="false" aria-controls="mr-sidebar-menu">
@@ -30,7 +40,7 @@ $mr_sidebar_links = [
   <div class="mr-sidebar__menu" id="mr-sidebar-menu">
     <nav class="mr-sidebar__nav">
       <?php foreach ($mr_sidebar_links as $key => $link): ?>
-        
+        <a
           class="mr-sidebar__link<?= $key === $active ? ' is-active' : '' ?>"
           href="<?= htmlspecialchars($link['href']) ?>"
           title="<?= htmlspecialchars($link['label']) ?>"
@@ -44,18 +54,20 @@ $mr_sidebar_links = [
     </nav>
 
     <div class="mr-sidebar__footer">
-      
-        <a class="mr-sidebar__link mr-sidebar__link--profile<?= $active === 'profile' ? ' is-active' : '' ?>"
+      <a
+        class="mr-sidebar__link mr-sidebar__link--profile<?= $active === 'profile' ? ' is-active' : '' ?>"
         href="profile.php"
         title="Nimal Perera"
         aria-label="Profile"
         <?= $active === 'profile' ? 'aria-current="page"' : '' ?>
-        >
+      >
         <span class="mr-avatar mr-avatar--dash">N</span>
         <span class="mr-sidebar__label">Profile</span>
       </a>
-      <a class="mr-sidebar__link" href="sign-in.php" title="Log out" aria-label="Log out">
-        <img src="https://img.icons8.com/ios-filled/50/454655/export.png" alt="">
+      <a class="mr-sidebar__link mr-sidebar__link--danger" href="sign-in.php" title="Log out" aria-label="Log out">
+        <span class="mr-sidebar__icon-box">
+          <img src="https://img.icons8.com/ios-filled/50/ff0000/export.png" alt="">
+        </span>
         <span class="mr-sidebar__label">Log out</span>
       </a>
     </div>
